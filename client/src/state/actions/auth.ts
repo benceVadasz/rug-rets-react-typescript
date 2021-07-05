15c691aa7ead7c7ab6651875f8662eb4556c5dd6
vadaszbence <64799151+vadaszbence@users.noreply.github.com>
2021-07-05 16:50:56 +0200

    Adds dispatch type to actions

--- client/src/state/actions/auth.ts
+++ client/src/state/actions/auth.ts
@@ -1,17 +1,17 @@
-import {AUTH, EDIT} from '../constants/actionTypes';
+import {AUTH} from '../constants/actionTypes';
 import * as api from '../../api';
-import {signUpData, signInData, updatedUser} from "../../types";
+import {AuthAction, EDIT, SIGN_IN, SIGN_UP, signInData, signUpData, updatedUser} from "../../types";
 import {ThunkAction} from "redux-thunk";
 import {RootState} from "../store";
-import {AuthAction} from "../../types";
+import {Dispatch} from "redux";
 
 
 export const signIn = (formData: signInData, history: any): ThunkAction<void, RootState, null,AuthAction> =>
-    async (dispatch: any) => {
+    async (dispatch: Dispatch<AuthAction>) => {
     try {
         const { data } = await api.signIn(formData);
 
-        dispatch({ type: AUTH, data })
+        dispatch({ type: SIGN_IN, payload: data})
         history.push('/');
     }
     catch (error) {
@@ -20,11 +20,11 @@ export const signIn = (formData: signInData, history: any): ThunkAction<void, Ro
 }
 
 export const signUp = (formData: signUpData, history: any): ThunkAction<void, RootState, null,AuthAction> =>
-    async (dispatch: any) => {
+    async (dispatch: Dispatch<AuthAction>) => {
     try {
         const { data } = await api.signUp(formData);
 
-        dispatch({ type: AUTH, data })
+        dispatch({ type: SIGN_UP, payload: data })
         history.push('/');
     }
     catch (error) {
@@ -33,10 +33,10 @@ export const signUp = (formData: signUpData, history: any): ThunkAction<void, Ro
 }
 
 export const editProfile = (formData: updatedUser, id: string): ThunkAction<void, RootState, null,AuthAction> =>
-    async (dispatch: any) => {
+    async (dispatch: Dispatch<AuthAction>) => {
     try {
         const { data } = await api.editProfile(formData, id);
-        dispatch({ type: AUTH, data })
+        dispatch({ type: EDIT, payload: data })
     }
     catch (error) {
         console.log(error.message)
--- client/src/state/actions/colors.ts
+++ client/src/state/actions/colors.ts
@@ -1,12 +1,46 @@
 import {CREATE, DELETE, FETCH_ALL, SET, SET_TYPE} from '../constants/actionTypes';
 
-import * as api from '../../api/index.js';
+import * as api from '../../api';
+import {Color, ColorAction, UploadColorData} from "../../types";
+import {RootState, useAppDispatch} from "../store";
+import {ThunkAction} from "redux-thunk";
+import {Dispatch} from "redux"
+import {Col} from "antd";
 
 export const getColors = (userId: string) => async (dispatch: any) => {
     try {
         const {data} = await api.fetchColors(userId);
 
         dispatch({type: FETCH_ALL, payload: data});
+    } catch (error) {
+        console.log(error.message);
+    }
+};
+
+export const uploadColor = (colorData: UploadColorData): ThunkAction<void, RootState, null, ColorAction> =>
+    async (dispatch: Dispatch<ColorAction>) => {
+    try {
+        const {data} = await api.uploadColor(colorData);
+
+        dispatch({type: CREATE, payload: data});
+    } catch (error) {
+        console.log(error.message);
+    }
+};
+
+export const colorExists = (hex: string) =>
+    async (dispatch: Dispatch<ColorAction>) => {
+    try {
+        return await api.checkIfColorExists(hex)
+    } catch (error) {
+        console.log(error.message);
+    }
+};
+
+export const setColorSelectionType = (type: string) =>
+    async (dispatch: Dispatch<ColorAction>) => {
+    try {
+        dispatch({type: SET_TYPE, payload: type});
     } catch (error) {
         console.log(error.message);
     }
