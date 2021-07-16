125b3ae958746bc54bb97e9dbbdc418e00f61516
vadaszbence <64799151+vadaszbence@users.noreply.github.com>
2021-07-16 16:52:13 +0200

    Converts DesignCard to class component

--- client/src/components/DesignCard.tsx
+++ client/src/components/DesignCard.tsx
@@ -1,42 +1,43 @@
-import React, {FunctionComponent as FC} from 'react';
+import React, {Component, FunctionComponent as FC} from 'react';
 import {DesignType} from "../types";
 import {Card} from 'antd';
 import * as DS from "../styles/DesignCard.styles";
 import {CaretUpOutlined, HeartOutlined} from "@ant-design/icons";
 
 
-export interface DesignCardProps {
+type DesignCardProps = {
     design: DesignType,
 }
 
-const DesignCard: FC<DesignCardProps> = ({design}: DesignCardProps) => {
+class DesignCard extends Component<DesignCardProps> {
 
-    const {Meta} = Card;
-
-    return (
-        <DS.LinkWrapper to={`/design/${design.id}`}>
-            <DS.Paper
-                hoverable
-                bordered={false}
-                cover={<DS.Image alt="example" src={design.imageUrl}/>}
-            >
-                <Meta title={design.title} description={`by: ${design.userName}`}/>
-                <DS.HorizontalBox>
-                    <DS.VerticalBox>
-                        <DS.Text>Rank:</DS.Text>
-                        <CaretUpOutlined/>
-                        <DS.Text> {design.rank}</DS.Text>
-                    </DS.VerticalBox>
-                    <DS.VerticalBox>
-                        <DS.Text>Votes:</DS.Text>
-                        <HeartOutlined/>
-                        <DS.Text> {design.numVotes}</DS.Text>
-                    </DS.VerticalBox>
-                </DS.HorizontalBox>
-            </DS.Paper>
-        </DS.LinkWrapper>
-    )
-};
+    render() {
+        const {Meta} = Card;
+        return (
+            <DS.LinkWrapper to={`/design/${this.props.design.id}`}>
+                <DS.Paper
+                    hoverable
+                    bordered={false}
+                    cover={<DS.Image alt="example" src={this.props.design.imageUrl}/>}
+                >
+                    <Meta title={this.props.design.title} description={`by: ${this.props.design.userName}`}/>
+                    <DS.HorizontalBox>
+                        <DS.VerticalBox>
+                            <DS.Text>Rank:</DS.Text>
+                            <CaretUpOutlined/>
+                            <DS.Text> {this.props.design.rank}</DS.Text>
+                        </DS.VerticalBox>
+                        <DS.VerticalBox>
+                            <DS.Text>Votes:</DS.Text>
+                            <HeartOutlined/>
+                            <DS.Text> {this.props.design.numVotes}</DS.Text>
+                        </DS.VerticalBox>
+                    </DS.HorizontalBox>
+                </DS.Paper>
+            </DS.LinkWrapper>
+        );
+    }
+}
 
 
 export default DesignCard;
