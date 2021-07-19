2414a671d26e4afd561fabdfce988137cb288347
vadaszbence <64799151+vadaszbence@users.noreply.github.com>
2021-07-19 11:23:56 +0200

    Adds shape images and svgs as components

---
+++ client/src/assets/rugShapes/rug.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export default function rug(props) {
+  return (
+    <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 368 368" style={{enableBackground: 'new 0 0 368 368'}} xmlSpace="preserve" width={368} height={368} {...props}>
+      <g>
+        <g>
+          <path d="M328,64H40C17.944,64,0,81.944,0,104v160c0,22.056,17.944,40,40,40h288c22.056,0,40-17.944,40-40V104
+          			C368,81.944,350.056,64,328,64z M352,264c0,13.232-10.768,24-24,24H40c-13.232,0-24-10.768-24-24v-8h336V264z M352,240H16V128h336
+          			V240z M352,112H16v-8c0-13.232,10.768-24,24-24h288c13.232,0,24,10.768,24,24V112z" />
+        </g>
+      </g>
+      <g/>
+      <g/>
+      <g/>
+      <g/>
+      <g/>
+      <g/>
+      <g/>
+      <g/>
+      <g/>
+      <g/>
+      <g/>
+      <g/>
+      <g/>
+      <g/>
+      <g/>
+    </svg>
+  );
+}
